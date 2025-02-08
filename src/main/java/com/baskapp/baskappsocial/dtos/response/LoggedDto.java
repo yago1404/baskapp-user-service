7ab113a2f4e5422a4b0d90a57c6f71d0218d5f9a
Yago Taveiros Ferreira <ytf@ic.ufal.br>
2025-02-08 18:39:17 -0300

    feat: remove no args constructor

--- src/main/java/com/baskapp/baskappsocial/dtos/response/LoggedDto.java
+++ src/main/java/com/baskapp/baskappsocial/dtos/response/LoggedDto.java
@@ -2,12 +2,10 @@ package com.baskapp.baskappsocial.dtos.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class LoggedDto {
     private String token;
     private String refreshToken;
